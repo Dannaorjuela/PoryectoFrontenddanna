6ad3395655814ccea54c169e15b106ae54b1698e
dannaorjuela <dannamarcela04@hotmail.com>
2023-07-22 09:33:13 -0500

    Subida de taller

---
+++ src/conf_auth.js
@@ -0,0 +1,8 @@
+
+const conf_auth = {
+    domain: 'dev-5j8pwp75cd1awgz2.us.auth0.com',
+    clientId: 'MnVTHMt4jDLHC9yQD02re1oOk8KSBW3N',
+    redirectUri: window.location.origin
+}
+
+export default conf_auth
